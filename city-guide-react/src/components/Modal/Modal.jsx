8f9610dfea07d68c9b24f58e8f6a7115b215ea67
Murka <muratahmethanov@gmail.com>
2025-01-22 11:59:28 +0300

    fix: rename class to className

--- city-guide-react/src/components/Modal/Modal.jsx
+++ city-guide-react/src/components/Modal/Modal.jsx
@@ -10,23 +10,23 @@ const Modal = ({ active, setActive }) => {
       <div className="modal__content" onClick={(e) => e.stopPropagation()}>
         <h1>Заявка</h1>
         <form>
-          <div class="modal__group">
+          <div className="modal__group">
             <p for="name">Ваше имя:</p>
             <input type="text" id="name" name="name" required></input>
           </div>
-          <div class="modal__group">
+          <div className="modal__group">
             <p for="email">Ваш email:</p>
             <input type="email" id="email" name="email" required></input>
           </div>
-          <div class="modal__group">
+          <div className="modal__group">
             <p for="subject">Тема обращения:</p>
             <input type="text" id="subject" name="subject" required></input>
           </div>
-          <div class="modal__group">
+          <div className="modal__group">
             <p for="message">Описание проблемы:</p>
             <textarea id="message" name="message" rows="5" required></textarea>
           </div>
-          <button class="modal__button" type="submit">
+          <button className="modal__button" type="submit">
             Отправить заявку
           </button>
         </form>
